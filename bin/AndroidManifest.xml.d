4ff1415667c2cc973812f56d84d6305c420482af
cruiz94 <carlangas94cr@gmail.com>
2019-02-21 12:14:26 -0600

    Add files via upload

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Carlos Miguel\Desktop\Quiniela\bin\AndroidManifest.xml : \
+C:\Users\Carlos Miguel\Desktop\Quiniela\AndroidManifest.xml \
---
+++ bin/Quiniela-debug-unaligned.apk.d
@@ -0,0 +1,5 @@
+C:\Users\Carlos Miguel\Desktop\Quiniela\bin\Quiniela-debug-unaligned.apk : \
+C:\Users\Carlos Miguel\Desktop\Quiniela\bin\Quiniela.ap_ \
+C:\Users\Carlos Miguel\Desktop\Quiniela\bin\classes.dex \
+C:\Users\Carlos Miguel\Desktop\Quiniela\libs\android-support-v4.jar \
+C:\Users\Carlos Miguel\Desktop\Quiniela\libs\android-support-v7-appcompat.jar \
